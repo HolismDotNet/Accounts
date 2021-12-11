ec2222e4d286ff24823e7430361e57d47bd213d6
Saeed Nemati <sn.1361@gmail.com>
2021-12-11 17:14:52 +0330

    committed by script

--- Api/Admin/UserController.cs
+++ Api/Admin/UserController.cs
@@ -1,21 +1,14 @@
-using Holism.Api;
-using Holism.Business;
-using Holism.Accounts.Business;
-using Holism.Accounts.Models;
-using Microsoft.AspNetCore.Mvc;
+namespace Holism.Accounts.AdminApi;
 
-namespace Holism.Accounts.AdminApi
+public class UserController : ReadController<UserView>
 {
-    public class UserController : ReadController<UserView>
+    public override ReadBusiness<UserView> ReadBusiness => new UserViewBusiness();
+
+    [HttpPost]
+    public IActionResult Sync()
     {
-        public override ReadBusiness<UserView> ReadBusiness => new UserViewBusiness();
-
-        [HttpPost]
-        public IActionResult Sync()
-        {
-            UserBusiness.SyncUsers();
-            return OkJson();
-        }
-
+        UserBusiness.SyncUsers();
+        return OkJson();
     }
+
 }
